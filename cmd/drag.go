332b32727a7a08e6d703fa237547867dee9c1d96
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-06-16 12:46:17 -0700

    Added mouse drag events

---
+++ cmd/drag.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/jphsd/glui"
+	"image"
+)
+
+// Example use of MouseDragListener
+
+func main() {
+	width, height := 600, 600
+
+	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
+	win := glui.NewGLWin(width, height, "Mouse Drag Test", img, true)
+	glui.NewMouseDragListener(win, glfw.MouseButtonLeft, func(pt []float64, act glfw.Action) {
+		switch act {
+		case glfw.Press:
+			fmt.Printf("Drag started at %.2f,%.2f\n", pt[0], pt[1])
+		case glfw.Release:
+			fmt.Printf("Drag ended at %.2f,%.2f\n", pt[0], pt[1])
+		case glfw.Repeat:
+			fmt.Printf("Dragging %.2f,%.2f\n", pt[0], pt[1])
+		}
+	})
+	glui.Loop(nil)
+}
